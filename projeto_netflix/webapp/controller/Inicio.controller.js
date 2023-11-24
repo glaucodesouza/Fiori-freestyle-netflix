c30e71e93d899988becba18bc1cfbdfe8b19e43e
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2023-11-23 21:46:18 -0300

    segunda aula

--- projeto_netflix/webapp/controller/Inicio.controller.js
+++ projeto_netflix/webapp/controller/Inicio.controller.js
@@ -7,7 +7,53 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("projetonetflix.controller.Inicio", {
-      onInit: function () {},
+      onInit: function () {
+        // Equivale ao INITIALIZATION do ABAP
+        // Evento que é executado quando o programa é iniciado
+
+        // Trabalhando com variáveis
+        let serie = "Os Trapalhões";
+        let ano = 1985;
+
+        let elenco = ["Didi", "Dedé", "Mussum", "Zacarias"];
+
+        console.log(serie);
+        console.log(ano);
+        console.log(elenco);
+
+        let liveAction = {
+          nome: "One Piece",
+          ano : 2023,
+          elenco : ["Luffy", "Zoro", "Nami", "Usopp"]
+        };
+
+        console.log(liveAction);
+
+        let minhaLista = [
+          {
+            nome: "One Piece",
+            ano : 2023,
+            elenco : ["Luffy", "Zoro", "Nami", "Usopp"]
+          },
+          {
+            nome: "Os Trapalhões",
+            ano : 1985,
+            elenco : ["Didi", "Dedé", "Mussum", "Zacarias"]
+          }
+        ];
+
+        console.log(minhaLista);
+
+        
+        //site do API:
+        // https://rapidapi.com/hub
+        // https://rapidapi.com/Glavier/api/netflix54/
+
+        //site do JSON viewr
+        //https://jsonviewer.stack.hu/
+
+
+      },
 
       onPressLinkInicio: function () {
         alert("Voce clicou em Inicio");
@@ -15,7 +61,7 @@ sap.ui.define(
 
       onPressLinkSeries: function () {
         alert("Voce clicou em Series");
-      },
+      }
     });
   }
 );
